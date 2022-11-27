2b45bd2bc8d090d464d91fa31e5efee32fa03b66
AlexandrePre <pre.alexandre1@gmail.com>
2022-11-27 11:11:44 +0100

    media query

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import Chrono from "./components/Chrono/Chrono";
 import Transition from "./Transition";
 import "./App.css";
+import Bgmediaquery from "./BGmediaquery";
 
 function App() {
   const [priceLevel, setPriceLevel] = useState(0);
   const [population, setPopulation] = useState(0);
   return (
     <div className="App">
+      <Bgmediaquery />
       <Router>
         <Transition
           priceLevel={priceLevel}
---
+++ src/BGmediaquery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Bgmedia from "./assets/img/mediaportable.png";
+import "./App.css";
+
+function Bgmediaquery() {
+  return (
+    <div className="Bgmedia">
+      <img src={Bgmedia} alt="bgmedia" id="media" />
+    </div>
+  );
+}
+
+export default Bgmediaquery;
